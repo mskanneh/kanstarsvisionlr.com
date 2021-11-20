51cc585afd4105b9a78121cab6e39f74ef540ac3
mskannehieee <mskanneh@ieee.org>
2021-11-20 12:33:29 -0500

    Additional Modification to source codes

--- asset/js/app.js
+++ asset/js/app.js
@@ -30,7 +30,18 @@ window.onload = () => {
   } else {
     document.querySelector(".header .header-sub2").classList.remove("active");
   }
+  fadeOut();
 };
+
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('active');
+  
+}
+
+function fadeOut(){
+  setTimeout(loader, 4000);
+}
 
 var swiper = new Swiper(".products-slider", {
   loop: true,
@@ -83,6 +94,55 @@ var swiper = new Swiper(".featured-slider", {
 
 var swiper = new Swiper(".arrivals-slider", {
   spaceBetween: 10,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+
+
+var swiper = new Swiper(".reviews-slider", {
+  spaceBetween: 10,
+  grabCursor:true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+
+var swiper = new Swiper(".blogs-slider", {
+  spaceBetween: 10,
+  grabCursor:true,
   loop: true,
   centeredSlides: true,
   autoplay: {
